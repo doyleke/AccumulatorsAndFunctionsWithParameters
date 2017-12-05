b6d09b402c688490f105266168ef16bc4ed85437
doyleke <doyleke@rose-hulman.edu>
2017-12-05 12:52:40 -0500

    working on m6

--- src/m6_summing.py
+++ src/m6_summing.py
@@ -17,7 +17,7 @@ def main():
 def run_test_sum_cosines():
     """ Tests the   sum_cosines   function. """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement this function.
+    # DONE: 2. Implement this function.
     #   It TESTS the  sum_cosines  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -30,9 +30,22 @@ def run_test_sum_cosines():
     print('--------------------------------------------------')
 
     # Test 1
-    expected = 1
+    answer = sum_cosines(3)
+    expected = ('cos(0) + cos(1) + cos(3)')
+    print('Test 1 expected:', expected)
+    print('       actual:  ', answer)
 
+    # Test 2
+    answer = sum_cosines(4)
+    expected =
+    print('Test 2 expected:', expected)
+    print('       actual:  ', answer)
 
+    # Test 3
+    answer = sum_cosines(5)
+    expected =
+    print('Test 3 expected:', expected)
+    print('       actual:  ', answer)
 
 def sum_cosines(n):
     """
@@ -45,13 +58,15 @@ def sum_cosines(n):
         cos(0) + cos(1) + cos(2) + cos(3)   which is about 0.13416.
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #   That is called TEST-DRIVEN DEVELOPMENT (TDD).
     #
     #   No fair running the code of  sum_cosines  to GENERATE
     #   test cases; that would defeat the purpose of TESTING!
     # ------------------------------------------------------------------
+
+    cos =
 
 
 def run_test_sum_square_roots():
